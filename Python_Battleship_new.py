49b647a8641e9e16bf0b81fe81c538444f8e1ddc
Areum Seo <fourcypresses@gmail.com>
2018-07-09 21:15:54 +0900

    First commit

--- Python_Battleship_new.py
+++ Python_Battleship_new.py
@@ -21,20 +21,19 @@ ship_row = random_row(board)
 ship_col = random_col(board)
 print ship_row
 print ship_col
-
 guess_row = int(raw_input("Guess Row: "))
 guess_col = int(raw_input("Guess Col: "))
 
 # Write your code below!
 if guess_row == ship_row and guess_col == ship_col:
-  print "Congratulations! You sank my battleship!"
-else: 
+  print "Congratulations! You sank my battleship!"   
+else:
   if guess_row not in range(5) or \
-     guess_col not in range(5):
-      print "Oops, that's not even in the ocean."
-  elif: 
-     guess_row == ""
-  else: 
-      print "You missed my battleship!"
-  board[guess_row][guess_col] = "X"
+    guess_col not in range(5):
+    print "Oops, that's not even in the ocean."
+  elif (board[guess_row][guess_col] == 'X'):
+    print "You guessed that one already."
+  else:
+    print "You missed my battleship!"
+    board[guess_row][guess_col] = "X"
   print_board(board)
